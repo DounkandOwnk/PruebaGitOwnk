4c44c5173c589b49b0bf2c4619d8206b5aefb200
DounkandOwnk <62023075+DounkandOwnk@users.noreply.github.com>
2020-03-10 16:21:33 -0500

    Se agrego package Herencia con 3 archvios java dando ejemplo sobre herencia

---
+++ Prueba_3/src/Herencia/Clase_Hija1.java
@@ -0,0 +1,17 @@
+package Herencia;
+
+public class Clase_Hija1 extends Clase_Padre{
+
+	private String nombrehija;
+
+	public String getNombrehija() {
+		return nombrehija;
+	}
+
+	public void setNombrehija(String nombrehija) {
+		this.nombrehija = nombrehija;
+	}
+	
+	
+	
+}
---
+++ Prueba_3/src/Herencia/Main.java
@@ -0,0 +1,19 @@
+package Herencia;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Clase_Hija1 ejemploHija = new Clase_Hija1();
+		ejemploHija.setNombre("Antonio");
+		ejemploHija.setApellido("Ramirez");
+		ejemploHija.setEdad(40);
+		ejemploHija.setNombrehija("Maria");
+
+		System.out.println("El papá de "+ejemploHija.getNombrehija()+" se llama "+ejemploHija.getNombre());
+		System.out.println("El papá de "+ejemploHija.getNombrehija()+" se apellida "+ejemploHija.getApellido());
+		System.out.println("El papá de "+ejemploHija.getNombrehija()+" tiene "+ejemploHija.getEdad());
+		System.out.println("La hija de "+ejemploHija.getNombre()+" se llama "+ejemploHija.getNombrehija());
+	}
+
+}
