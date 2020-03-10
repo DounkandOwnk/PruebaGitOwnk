925f1e40fa6f24cfce8b3e695498defb33c47529
DounkandOwnk <62023075+DounkandOwnk@users.noreply.github.com>
2020-03-10 10:19:25 -0500

    Add files via upload
    Se sube Proyecto Java

---
+++ Prueba_3/src/Prueba_3/Prueba_3.java
@@ -0,0 +1,15 @@
+package Prueba_3;
+
+import java.util.Scanner;
+
+public class Prueba_3 {
+
+public static void main(String[]arg){
+
+	String Var="Hola Mundo";
+	
+	System.out.println(Var);
+
+}
+	
+}
